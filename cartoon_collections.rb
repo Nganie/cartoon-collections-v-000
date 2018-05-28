1c1e9011db68b708ebebf2b1a73a31af1c24c94b
Nganie <ngan.tp.quan@gmail.com>
2018-05-28 07:21:57 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,5 +13,5 @@ end
 def find_the_cheese(queso)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  queso.include?("cheese_types")
+  queso.include? {|x| x == "cheese_types"}
 end
