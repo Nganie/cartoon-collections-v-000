6cc527b3305a6b5aea40f4f82f39a9f77ed5c037
Nganie <ngan.tp.quan@gmail.com>
2018-05-28 06:45:31 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,7 @@
 def roll_call_dwarves(dwarves)# code an argument here
-  # Your code here
+  names = []
+  dwarves.each do |name|
+    dwarves.push("#{name}")# Your code here
 end
 
 def summon_captain_planet# code an argument here
